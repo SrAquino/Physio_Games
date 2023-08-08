2404d15bb7d18e4d6269111bf509f36bb036b010
SrAquino <douglasaquino4@gmail.com>
2023-08-08 19:27:27 -0300

    feat: projeto iniciado, pagina web hospedada

---
+++ Controlador/Main/Main.ino
@@ -0,0 +1,11 @@
+// Lê os dados do módulo acelerômetro/giroscópio e envia via bluetooth para o sistema desenvolvido na Godot
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
