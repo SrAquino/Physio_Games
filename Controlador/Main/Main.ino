e6782982792ab7d080661c6a3b71ad67f24e10bf
SrAquino <douglasaquino4@gmail.com>
2023-10-04 11:00:13 -0300

    pag web e novo jogo

--- Controlador/Main/Main.ino
+++ Controlador/Main/Main.ino
@@ -1,11 +1,68 @@
-// Lê os dados do módulo acelerômetro/giroscópio e envia via bluetooth para o sistema desenvolvido na Godot
+#include <BleKeyboard.h>
+#include <MPU6050_tockn.h>
+#include <Wire.h>
+#include <TaskScheduler.h>
+
+BleKeyboard bleKeyboard;
+MPU6050 mpu6050(Wire);
+Scheduler runner;
+
+void GyroTaskCallback();
+void BluetoothTaskCallback();
+
+
+Task GyroTask(100, TASK_FOREVER, &GyroTaskCallback);
+Task BluetoothTask(100, TASK_FOREVER, &BluetoothTaskCallback);
+
+
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+  bleKeyboard.begin();
 
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
+
+  // Agende as tarefas para serem executadas em paralelo
+  runner.init();
+  runner.addTask(GyroTask);
+  runner.addTask(BluetoothTask);
+  GyroTask.enable();
+  BluetoothTask.enable();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  runner.execute();
+}
+
+void GyroTaskCallback() {
+  mpu6050.update();
 
 }
+
+void BluetoothTaskCallback() {
+  if (bleKeyboard.isConnected()) {
+
+    if(mpu6050.getAngleZ() > 0){
+        
+      if(mpu6050.getAngleX() < 0) {
+        bleKeyboard.write(KEY_LEFT_ARROW);
+        Serial.println("Esquerda");
+      } 
+      else {
+        bleKeyboard.write(KEY_RIGHT_ARROW);
+        Serial.println("Direita");
+      } 
+    } else {
+      Serial.print("Mantenha Z maior que 0 para iniciar: ");
+      Serial.println(mpu6050.getAngleZ());
+    }
+
+  } else {
+    Serial.println("Waiting...");
+  }
+}
+
